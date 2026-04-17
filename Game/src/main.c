7de8b2e08262035741c1a63b6d56d924a05f72c8
junchengye <juncheng.ye@estudiantat.upc.edu>
2026-04-17 08:41:36 +0200

    mapa new

--- Game/src/main.c
+++ Game/src/main.c
@@ -11,18 +11,18 @@
 
 
 int map[MAP_HEIGHT][MAP_WIDTH] = {
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
 #define SPRING_WIDTH  32
@@ -54,75 +54,92 @@ int main() {
     Texture2D animL = LoadTexture("Anim_L.png");
     Texture2D fondo = LoadTexture("maapa.png");
 
-    if (fondo.id == 0) {
-        TraceLog(LOG_ERROR, "No se pudo cargar la imagen 'castlevania.png'");
-    }
     SetTextureFilter(fondo, TEXTURE_FILTER_POINT);
 
-    float playerX = 200.0f;
-    float playerY = 0.0f;
-    float velocityY = 0.0f;
+    float playerX = 200;
+    float playerY = 0;
+    float velocityY = 0;
+
     const float GRAVITY = 0.8f;
     const float JUMP_FORCE = -12.0f;
+
     bool isGrounded = false;
     int direction = 0;
     bool isMoving = false;
     float moveSpeed = 3.0f;
 
     Spring.Speed = 5;
-    Spring.Frame = 0;
-    Spring.Counter = 0;
 
     Camera2D camera = { 0 };
-    camera.offset = (Vector2){ SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f };
+    camera.offset = (Vector2){ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
     camera.zoom = 2.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+
         AnimationSettings();
 
+        float newX = playerX;
         isMoving = false;
+
+        // Movimiento horizontal
         if (IsKeyDown(KEY_D)) {
-            playerX += moveSpeed;
+            newX += moveSpeed;
             direction = 0;
             isMoving = true;
         }
         if (IsKeyDown(KEY_A)) {
-            playerX -= moveSpeed;
+            newX -= moveSpeed;
             direction = 1;
             isMoving = true;
         }
 
+        // ===== COLISION HORIZONTAL =====
+        int topTile = (int)(playerY) / TILE_SIZE;
+        int bottomTile = (int)(playerY + SPRING_HEIGHT - 1) / TILE_SIZE;
+
+        // Derecha
+        int rightTile = (int)(newX + SPRING_WIDTH) / TILE_SIZE;
+        for (int y = topTile; y <= bottomTile; y++) {
+            if (map[y][rightTile] == 1) {
+                newX = rightTile * TILE_SIZE - SPRING_WIDTH;
+            }
+        }
+
+        // Izquierda
+        int leftTile = (int)(newX) / TILE_SIZE;
+        for (int y = topTile; y <= bottomTile; y++) {
+            if (map[y][leftTile] == 1) {
+                newX = (leftTile + 1) * TILE_SIZE;
+            }
+        }
+
+        playerX = newX;
+
+        // ===== GRAVEDAD =====
         velocityY += GRAVITY;
         playerY += velocityY;
 
+        // ===== COLISION SUELO =====
         int tileX = (int)(playerX + SPRING_WIDTH / 2) / TILE_SIZE;
         int tileY = (int)(playerY + SPRING_HEIGHT) / TILE_SIZE;
-        if (tileY >= MAP_HEIGHT) tileY = MAP_HEIGHT - 1;
-        if (tileX >= 0 && tileX < MAP_WIDTH && tileY >= 0 && tileY < MAP_HEIGHT) {
-            if (map[tileY][tileX] == 1) {
-                playerY = tileY * TILE_SIZE - SPRING_HEIGHT;
-                velocityY = 0;
-                isGrounded = true;
-            }
-            else {
-                isGrounded = false;
-            }
+
+        if (map[tileY][tileX] == 1) {
+            playerY = tileY * TILE_SIZE - SPRING_HEIGHT;
+            velocityY = 0;
+            isGrounded = true;
         }
         else {
             isGrounded = false;
         }
 
+        // Salto
         if (isGrounded && IsKeyPressed(KEY_SPACE)) {
             velocityY = JUMP_FORCE;
-            isGrounded = false;
         }
 
-        if (playerX < 0) playerX = 0;
-        if (playerX > SCREEN_WIDTH - SPRING_WIDTH) playerX = SCREEN_WIDTH - SPRING_WIDTH;
-
-        camera.target = (Vector2){ playerX + SPRING_WIDTH / 2, playerY + SPRING_HEIGHT / 2 };
+        camera.target = (Vector2){ playerX, 220 };
 
         BeginDrawing();
         ClearBackground(BLACK);
@@ -131,6 +148,7 @@ int main() {
 
         DrawTextureEx(fondo, (Vector2) { 0, 0 }, 0, 2.0f, WHITE);
 
+        // Dibujar mapa
         for (int y = 0; y < MAP_HEIGHT; y++) {
             for (int x = 0; x < MAP_WIDTH; x++) {
                 if (map[y][x] == 1) {
@@ -139,42 +157,26 @@ int main() {
             }
         }
 
-        Rectangle sourceRect = { Spring.Frame * SPRING_WIDTH, 0, SPRING_WIDTH, SPRING_HEIGHT };
-        Rectangle destRect = { playerX, playerY, SPRING_WIDTH, SPRING_HEIGHT };
-        Vector2 origin = { 0, 0 };
+        Rectangle source = { Spring.Frame * SPRING_WIDTH, 0, SPRING_WIDTH, SPRING_HEIGHT };
+        Rectangle dest = { playerX, playerY, SPRING_WIDTH, SPRING_HEIGHT };
 
         if (isMoving) {
-            if (direction == 0) {
-                DrawTexturePro(animR, sourceRect, destRect, origin, 0, WHITE);
-            }
-            else {
-                DrawTexturePro(animL, sourceRect, destRect, origin, 0, WHITE);
-            }
+            if (direction == 0)
+                DrawTexturePro(animR, source, dest, (Vector2) { 0, 0 }, 0, WHITE);
+            else
+                DrawTexturePro(animL, source, dest, (Vector2) { 0, 0 }, 0, WHITE);
         }
         else {
-            if (direction == 0) {
-                DrawTextureEx(rabbitIdleR, (Vector2) { playerX, playerY }, 0, 1.0f, WHITE);
-            }
-            else {
-                DrawTextureEx(rabbitIdleL, (Vector2) { playerX, playerY }, 0, 1.0f, WHITE);
-            }
+            if (direction == 0)
+                DrawTextureEx(rabbitIdleR, (Vector2) { playerX, playerY }, 0, 1, WHITE);
+            else
+                DrawTextureEx(rabbitIdleL, (Vector2) { playerX, playerY }, 0, 1, WHITE);
         }
 
         EndMode2D();
-
-        if (fondo.id == 0) {
-            DrawText("ERROR: No se cargo la imagen 'castlevania.png'", 10, 10, 20, RED);
-        }
-
         EndDrawing();
     }
 
-    UnloadTexture(rabbitIdleR);
-    UnloadTexture(rabbitIdleL);
-    UnloadTexture(animR);
-    UnloadTexture(animL);
-    UnloadTexture(fondo);
     CloseWindow();
-
     return 0;
 }
